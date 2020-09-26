0f5fd4da1ab6752888bc6f36e88fe88e282bdfae
Kunal Sinha <kunalsinha98@gmail.com>
2020-09-25 22:34:50 -0400

    added antd

--- healthy-convos/src/App.js
+++ healthy-convos/src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Button } from 'antd';
+
 
 function App() {
   return (
@@ -18,6 +20,7 @@ function App() {
         >
           Learn React
         </a>
+        <Button type="primary">Button</Button>
       </header>
     </div>
   );
