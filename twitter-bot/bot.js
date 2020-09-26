a5094a7ca56ee3a6d20a7344eacb3b2c791586b5
Yash Gharat <yashgharat12@gmail.com>
2020-09-26 11:59:24 -0400

    twitter-bot

---
+++ twitter-bot/bot.js
@@ -0,0 +1,17 @@
+const twit = require('twit')
+const config = require('./config')
+const base_url = "https://api.twitter.com/"
+const client = new twit(config)
+
+async function getSample() {
+    var stream = client.stream('statuses/filter', { track: 'tiktok' })
+    stream.on('tweet', function (tweet) {
+        console.log(tweet)
+        sleep()
+      })
+}
+getSample()
+
+async function sleep() {
+    await(10000);
+}
