97e3319d2914c7f3a2b61bb8a2249e83d7fb39fb
Aryan Jha <aryanjha@gmail.com>
2020-09-27 09:28:36 -0400

    final styling

--- healthy-convos/src/Components/HomePage.js
+++ healthy-convos/src/Components/HomePage.js
@@ -10,11 +10,11 @@ class HomePage extends Component {
   render() {
     return (
       <div>
-          <Title level={1} style={{margin: "auto", marginBottom:"60px"}}> Starting #HealthyConversations, One Tweet at a Time.</Title>
+          <Title level={1} style={{margin: "auto", marginBottom:"60px"}}><Text type="secondary">Starting <Text strong>#HealthyConversations</Text>, One Tweet at a Time.</Text></Title>
         <Row>
           <Col sm={36} md={24} lg={12}>
             <Row>
-              <Title level={4} style={{margin: "auto", marginBottom:"10px"}}> Articles and Videos for you</Title>
+              <Title level={2} style={{margin: "auto", marginBottom:"10px"}}> Articles and Videos</Title>
               <Col md={24} lg={16} style={{backgroundColor: "#ff9c6e", borderRadius:"20px", margin: "auto", padding:"20px", marginBottom:"20px"}}>
                 <Articles/>
               </Col>
@@ -35,16 +35,16 @@ class HomePage extends Component {
           </Col>
           </Row>
           <Row>
-            <Title level={2} style={{margin: "auto", marginTop:"50px"}}> About our Bot</Title>
+            <Title level={2} style={{margin: "auto", marginTop:"50px"}}> <Text keyboard>About our Bot</Text></Title>
           </Row>
           <Row>
-            <Title level={5} style={{margin: "left", marginTop:"20px"}}>Our goal was to use the Twitter API to promote tweets that are examples of healthy conversations.</Title>
+            <Title level={5} style={{margin: "left", marginTop:"20px"}}> <Text keyboard>Our goal was to use the Twitter API to promote tweets that are examples of healthy conversations.</Text></Title>
           </Row>
           <Row>
-            <Title level={5} style={{margin: "left"}}>We decided to use Microsoft's Sentiment Analysis API on tweets on the top 3 trending topics to determine if a tweet was an example of a healthy conversation</Title>
+            <Title level={5} style={{margin: "left"}}> <Text keyboard>We decided to use Microsoft's Sentiment Analysis API on tweets on the top 3 trending topics to determine if a tweet was an example of a healthy conversation</Text></Title>
           </Row>
           <Row>
-            <Title level={5} style={{margin: "left"}}>Based on the score provided by the Sentiment Anaylsis API, our Twitter Bot would tweet about that post with its score, the link to the tweet, and the #HealthyConversation</Title>
+            <Title level={5} style={{margin: "left"}}> <Text keyboard>Based on the score provided by the API, our Twitter Bot would tweet about that post with its score, the link to the tweet, and the #HealthyConversation</Text></Title>
           </Row>
       </div>
     );
